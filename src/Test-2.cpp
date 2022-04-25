a863262bec4723c5efd458fdf146dc52ef9365e6
Michael Iro <michael.iro@tuwien.ac.at>
2022-04-25 16:17:53 +0200

    Clean Test-*.cpp

--- src/Test-2.cpp
+++ src/Test-2.cpp
@@ -59,3 +59,39 @@ int main() {
 }
 
 
+/**
+ * Old
+ * 
+ *  Shadow3API shadow_((char*) "../test-data/in/shadow3");
+    PolyCapAPI pc1_((char*) "../test-data/in/polycap/pc-236-descr-turned.txt");
+    //PolyCapAPI pc1_((char*) "../test-data/in/polycap/pc-246-descr.txt");
+
+    unsigned int seed_ = chrono::steady_clock::now().time_since_epoch().count();
+    boost::mt19937 rand_gen_(seed_);
+
+    shadow_.trace(100000000,rand_gen_());
+    arma::Mat<double> shadowBeam_ = shadow_.getBeamMatrix();
+
+    std::vector<double> energies = {6.5, 8.0, 10.0, 12.0, 14.0, 17.5, 20.0};
+   //pc1_.trace(shadowBeam_, 10000,"/media/miro/Data/Shadow-Beam/Transmission/PC-236/no-leak.h5", true);
+
+    for(int i = 5; i < 6; i++){
+        //double i = 6.5;
+        
+        //arma::Col<double> eneCol(shadowBeam_.n_rows, arma::fill::value(energies[i]*50677300.0));
+        //shadowBeam_.col(10) = eneCol; 
+
+        XRBeam beam_( pc1_.traceFast(shadowBeam_) );
+
+        std::string myID;
+        //if(i*0.2<10)
+        //    myID = "0"+std::to_string(i*0.2);
+        //else
+        //    myID = std::to_string(i*0.2);
+        myID = std::to_string(energies[i]);
+
+        beam_.getMatrix().save(arma::hdf5_name("/media/miro/Data/Shadow-Beam/Transmission/PC-236/new/beam-236-"+myID+"keV.h5", "my_data"));
+    }
+
+ * 
+ */
--- src/Test-3.cpp
+++ src/Test-3.cpp
@@ -1,57 +1,25 @@
 #include <iostream>
-#include "cuda/GPUTracer.cuh"
-//#include "api/PlotAPI.hpp"
-#include "base/XRBeam.hpp"
-#include "api/PolyCapAPI.hpp"
+#include <cuda_runtime.h>
+
+#include "cuda/RayGPU.cu"
+#include "cuda/ChemElementGPU.cu"
+#include "cuda/MaterialGPU.cu"
+#include "cuda/VoxelGPU.cu"
+#include "cuda/SampleGPU.cu"
+#include "cuda/TracerGPU.cuh"
+
+#include <armadillo>
+
 
 int main() {
+
 	std::cout << "START: Test-3" << std::endl;
-//---------------------------------------------------------------------------------------------
-    ChemElement cu(29);
-    ChemElement sn(50);
-	ChemElement pb(82);
-	map<ChemElement* const,double> bronzeMap{{&cu,0.7},{&sn,0.2},{&pb,0.1}};
+//---------------------------------------------------------------------------------------------	
 
-	//arma::field<Material> myMaterials(11,11,11); TODO: change vec<vec<vec>> to field
-	vector<vector<vector<Material>>> myMat;	
-	for(int i = 0; i < 11; i++){
-		vector<vector<Material>> myMat1;
-		for(int j = 0; j < 11; j++){
-			vector<Material> myMat2;
-			for(int k = 0; k < 11; k++){
-				myMat2.push_back(Material(bronzeMap,8.96));
-				//myMaterials(i,j,k) = Material(cuMatMap,8.96);
-			}
-			myMat1.push_back(myMat2);
-		}
-		myMat.push_back(myMat1);
-	} 
+	TracerGPU::callTrace(); 
 
 //---------------------------------------------------------------------------------------------
-
-	Sample sample_ (0.,0.,0.,150.,150.,5.,15.,15.,0.5,myMat);
-	//sample_.print();
-//---------------------------------------------------------------------------------------------
-
-	arma::Mat<double> myPrimaryCapBeam;
-    myPrimaryCapBeam.load(arma::hdf5_name("/media/miro/Data/Documents/TU Wien/VSC-BEAM/PrimaryBeam-1-0.h5","my_data"));
-	XRBeam myPrimaryBeam(myPrimaryCapBeam);
-//---------------------------------------------------------------------------------------------
-    (myPrimaryBeam.getRays())[0].print(9);
-
-    int N = 1<<20;
-    float *x, *y;
-
-    GPUTracer::callAdd(N, x, y,&sample_, &myPrimaryBeam);
-
-    //arma::Mat<double> dummy;
-    //PlotAPI::scatter_plot((char*) "../test-data/out/plots/example-sine-functions.pdf",true,true, dummy);
-
-
-
-
     std::cout << "END: Test-3" << std::endl;
+
     return 0;
 }
-
-
